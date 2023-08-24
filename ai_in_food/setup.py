0cb89ed21001d6c436c5833f63f2166f32fcd136
dhdzmota <dhdzmota@gmail.com>
2023-08-24 09:02:53 -0600

    Project structure

---
+++ ai_in_food/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='En México se desperdician alrededor de 28 millones de toneladas de alimentos al año. Según la SEDESOL,',
+    author='ITESO',
+    license='',
+)
